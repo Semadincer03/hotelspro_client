88d938c244c9c3bf8f0f613cafaf3f986bb318a8
Sema Dincer <dincersema@hotmail.com>
2016-09-28 17:50:33 +0300

    changed some files directory

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from book import BookProcessor
+from hotelspro_client.book import BookProcessor
 
 if __name__ == '__main__':
     params = {'checkin': '2016-09-30', 'checkout': '2016-10-03', 'pax': '1', 'destination_code': '11260',
               'client_nationality': 'tr', 'currency': 'USD'}
 
 
-    book_processor = BookProcessor(username, password, params=params)
+    book_processor = BookProcessor(username="semadincer", password="qwer321", params=params)
+    print book_processor.search()
