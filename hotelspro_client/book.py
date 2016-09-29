c3d88d9367a9515a3a07a911a2e1e9009d3a99d8
Sema Dincer <dincersema@hotmail.com>
2016-09-29 16:01:10 +0300

    ++

--- hotelspro_client/book.py
+++ hotelspro_client/book.py
@@ -16,6 +16,7 @@ class BookProcessor(object):
 
     def search(self, params):
         """Search method returns the results of searching parameters."""
+
         for item in params:
             self.join_url += (item + "=" + params[item] + "&")
         complete_url = '{}{}'.format(self.search_url, self.join_url)
@@ -26,12 +27,14 @@ class BookProcessor(object):
     def availability(self, code):
         """Availability method gets the product code
         that came from search method results and checks it's availability status"""
+
         r = requests.get(self.availability_url + "/" + code, auth=(self.username, self.password))
         if r.status_code == 200:
             return r.json()
 
     def provision(self, code):
         """Provision method gets the product code and makes post for the request """
+
         r = requests.post(self.provision_url + "/" + code, auth=(self.username, self.password))
         if r.status_code == 200:
             return r.json()
@@ -39,6 +42,7 @@ class BookProcessor(object):
     def book(self, code, post_data):
         """Book method takes 2 parameters.
         The code parameter is provision code and the post_data is a dictionary which have the info about name,pax etc."""
+
         if not code or not post_data:
             StandardError("provision code and postdata information\
                                              is required!")
@@ -50,11 +54,13 @@ class BookProcessor(object):
 
     def bookings(self, code=""):
         """Bookings method returns the books which is succeeded"""
+
         r = requests.get(self.booking_url + "/" + code, auth=(self.username, self.password))
         if r.status_code == 200:
             return r.json()
 
     def cancel(self, code):
         """Cancel method takes a parameter which is book code and make cancellation for this book. """
+
         r = requests.post(self.cancel_url + "/" + code, auth=(self.username, self.password))
         return r.json()
