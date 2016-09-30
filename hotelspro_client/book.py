b3765b2d2c6640aa68c5ac930ba53b729b4f50a8
Sema Dincer <dincersema@hotmail.com>
2016-09-30 10:36:15 +0300

    ++

--- hotelspro_client/book.py
+++ hotelspro_client/book.py
@@ -3,64 +3,79 @@ import requests
 
 class BookProcessor(object):
     def __init__(self, username, password):
-        main_url = 'http://localhost:8000/api/v2'
+        MAIN_URL = 'http://localhost:8000/api/v2'
         self.username = username
         self.password = password
-        self.search_url = main_url + '/search'
-        self.availability_url = main_url + '/availability'
-        self.provision_url = main_url + '/provision'
-        self.book_url = main_url + '/book'
-        self.cancel_url = main_url + '/cancel'
-        self.booking_url = main_url + '/bookings'
-        self.join_url = '/?'
+        self.SEARCH_URL = MAIN_URL + '/search/'
+        self.AVAILABILITY_URL = MAIN_URL + '/availability/'
+        self.PROVISION_URL = MAIN_URL + '/provision/'
+        self.BOOK_URL = MAIN_URL + '/book/'
+        self.CANCEL_URL = MAIN_URL + '/cancel/'
+        self.BOOKING_URL = MAIN_URL + '/bookings/'
+        self.join_url = '?'
 
     def search(self, params):
         """Search method returns the results of searching parameters."""
 
-        for item in params:
-            self.join_url += (item + "=" + params[item] + "&")
-        complete_url = '{}{}'.format(self.search_url, self.join_url)
-        r = requests.get(complete_url, auth=(self.username, self.password))
-        if r.status_code == 200:
-            return r.json()
+        if not params or not isinstance(params, dict):
+            raise StandardError("params information is required and must be a dictionary")
+        else:
+            for item in params:
+                self.join_url += (item + "=" + params[item] + "&")
+            complete_url = '{}{}'.format(self.SEARCH_URL, self.join_url)
+
+            r = requests.get(complete_url, auth=(self.username, self.password))
+            if r.status_code == 200:
+                return r.json()
 
     def availability(self, code):
         """Availability method gets the product code
         that came from search method results and checks it's availability status"""
 
-        r = requests.get(self.availability_url + "/" + code, auth=(self.username, self.password))
-        if r.status_code == 200:
-            return r.json()
+        if not code:
+            raise StandardError("availability method must take a product code")
+        else:
+            r = requests.get(self.AVAILABILITY_URL + code, auth=(self.username, self.password))
+            if r.status_code == 200:
+                return r.json()
 
     def provision(self, code):
         """Provision method gets the product code and makes post for the request """
 
-        r = requests.post(self.provision_url + "/" + code, auth=(self.username, self.password))
-        if r.status_code == 200:
-            return r.json()
+        if not code:
+            raise StandardError("provision method must take a product code")
+        else:
+            r = requests.post(self.PROVISION_URL + code, auth=(self.username, self.password))
+            if r.status_code == 200:
+                return r.json()
 
     def book(self, code, post_data):
         """Book method takes 2 parameters.
         The code parameter is provision code and the post_data is a dictionary which have the info about name,pax etc."""
 
         if not code or not post_data:
-            StandardError("provision code and postdata information\
-                                             is required!")
+            StandardError("provision code and post_data information is required!")
         if not isinstance(post_data, dict):
-            raise StandardError("postdata must be a dictionary!")
-        r = requests.post(self.book_url + "/" + code, post_data, auth=(self.username, self.password))
+            raise StandardError("post_data must be a dictionary!")
+
+        r = requests.post(self.BOOK_URL + code, post_data, auth=(self.username, self.password))
         if r.status_code == 200:
             return r.json()
 
     def bookings(self, code=""):
         """Bookings method returns the books which is succeeded"""
 
-        r = requests.get(self.booking_url + "/" + code, auth=(self.username, self.password))
+        r = requests.get(self.BOOKING_URL + code, auth=(self.username, self.password))
         if r.status_code == 200:
             return r.json()
 
     def cancel(self, code):
         """Cancel method takes a parameter which is book code and make cancellation for this book. """
 
-        r = requests.post(self.cancel_url + "/" + code, auth=(self.username, self.password))
-        return r.json()
+        if not code:
+            raise StandardError("cancel method must take the book code")
+        else:
+            r = requests.post(self.CANCEL_URL + code, auth=(self.username, self.password))
+            return r.json()
+
+
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from hotelspro_client.book import BookProcessor
 
 if __name__ == '__main__':
-    params = {'checkin': '2016-09-30', 'checkout': '2016-10-03', 'pax': '1', 'destination_code': '11260',
+    params = {'checkin': '2016-10-03', 'checkout': '2016-10-05', 'pax': '1', 'destination_code': '11260',
               'client_nationality': 'tr', 'currency': 'USD'}
 
-
-    book_processor = BookProcessor(usename, password)
+    book_processor = BookProcessor(username, password)
     print book_processor.search(params)
